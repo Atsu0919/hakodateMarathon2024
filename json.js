acb9937f62b945a56266d68fa9d1c7e98831ed4b
atsumini0919 <atsumini0919@gmail.com>
2023-06-15 14:12:15 +0900

    json.jsを作成

---
+++ json.js
@@ -0,0 +1,143 @@
+var firstAid ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第１救護所",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.772275,
+            41.775792
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第２救護所",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.73967,
+            41.769959
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第３救護所",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.720412,
+            41.760669
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第４救護所",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.726902,
+            41.786673
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第５救護所",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.709715,
+            41.769119
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第６救護所",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.739048,
+            41.783073
+          ]
+        }
+      }
+    ]
+  };
