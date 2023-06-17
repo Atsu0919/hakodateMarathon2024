69789be4f4c214905ebbd5076a69a3b0bd4c49fd
Atsu <atsumini0919@gmail.com>
2023-06-17 14:40:15 +0900

    関門データ追加

--- json.js
+++ json.js
@@ -141,3 +141,226 @@ var firstAid ={
       }
     ]
   };
+
+  var gate =
+  {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第２関門",
+          "comment":"閉鎖時刻10:39",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.790895,
+            41.7749
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第３関門",
+          "comment":"閉鎖時刻11:20",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.82756,
+            41.765942
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第４関門",
+          "comment":"閉鎖時刻12:00",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.772446,
+            41.776
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第５関門",
+          "comment":"閉鎖時刻12:38",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.720503,
+            41.760893
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第６関門",
+          "comment":"閉鎖時刻13:16",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.740512,
+            41.777065
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第７関門",
+          "comment":"閉鎖時刻13:48",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.720084,
+            41.769191
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第８関門",
+          "comment":"閉鎖時刻14:18",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.719725,
+            41.769031
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第９関門",
+          "comment":"閉鎖時刻14:41",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.726988,
+            41.782489
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "第１関門",
+          "comment":"閉鎖時刻10:14",
+          "_markerType": "Icon",
+          "_iconUrl": "https://maps.gsi.go.jp/portal/sys/v4/symbols/080.png",
+          "_iconSize": [
+            20,
+            20
+          ],
+          "_iconAnchor": [
+            10,
+            10
+          ]
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            140.765955,
+            41.781489
+          ]
+        }
+      }
+    ]
+  };
